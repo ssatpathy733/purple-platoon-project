d4e6ab0e904d43ddc7e8a3c3078b6e619529102a
Shreejita Satpathy <63432781+ssatpathy733@users.noreply.github.com>
2022-02-20 16:10:25 -0500

    audio

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ let gameOptions = {
     playerStartPosition: 200,
     jumps: 2
 }
- 
+
+function play() {
+    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
+    audio.play();
+    audio.loop = true; 
+  }
+
 window.onload = function() {
  
     // object containing configuration options
